020d90880c30a46fc1d24b95c6d4954cd7978cc8
devsk <srik3713@gmail.com>
2025-05-17 14:41:24 +0530

    white list removed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var cors_proxy = require('cors-anywhere');
 
 // creating cors proxy server
 cors_proxy.createServer({
-    originWhitelist: ["https://agri-lynx-git-test-devsk001s-projects.vercel.app","http://localhost:8080","https://agri-lynx-web.app"], // Allow all origins
+    originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, function() {
